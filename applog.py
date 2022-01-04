e1f8a21670affc78b857e3f7a88ce99cbbdd60f3
rajesh499 <83573991+rajesh499@users.noreply.github.com>
2022-01-04 20:58:01 +0530

    Initial commit

---
+++ applog.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import logging
+app = Flask(__name__)
+logging.basicConfig(filename='flask.log', level=logging.INFO)
+@app.route('/')
+def hello_world():
+    return 'rajesh'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
